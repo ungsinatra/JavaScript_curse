46fe113dd3f58d93c7f75e0d3f9507e2471da14b
Sardor <mamirov.21@mail.ru>
2021-10-29 14:40:00 +0300

    12 prac done!

--- prac.js
+++ prac.js
@@ -17,3 +17,41 @@
 //     sda = false;
 
 //  console.log(dsa || sda); 
+
+
+
+
+// Практическое задание 12 партика 
+
+
+
+
+//
+
+let numberOfFilms = +prompt("сколько фильмов вы посморели?", "");
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const
+    qw1 = prompt("последний фильм?", ""),
+    qw2 = prompt("На сколько вы его оцените?", ""),
+    qw3 = prompt("последний фильм?", ""),
+    qw4 = prompt("последний фильм?", "");
+
+
+personalMovieDB.movie[qw1] = qw2;
+personalMovieDB.movie[qw3] = qw4;
+
+
+console.log(personalMovieDB);
+
+
