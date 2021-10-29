3bfda0e378ef30d546b456203a0d8ace542267f7
Sardor <mamirov.21@mail.ru>
2021-10-29 15:09:43 +0300

    12, 13, prac and if, switch, условие if и switch

--- prac.js
+++ prac.js
@@ -17,3 +17,72 @@
 //     sda = false;
 
 //  console.log(dsa || sda); 
+
+
+
+
+// Практическое задание 12 партика 
+
+
+
+
+//
+
+let numberOfFilms = +prompt("сколько фильмов вы посморели?", "");
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const
+    qw1 = prompt("последний фильм?", ""),
+    qw2 = prompt("На сколько вы его оцените?", ""),
+    qw3 = prompt("последний фильм?", ""),
+    qw4 = prompt("последний фильм?", "");
+
+
+personalMovieDB.movie[qw1] = qw2;
+personalMovieDB.movie[qw3] = qw4;
+
+
+console.log(personalMovieDB);
+
+
+// 13 prac
+
+
+// let number = 100;
+// if (number > 100) {
+//     console.log("не больше!")
+
+// } else if (number < 50) {
+//     console.log("меньше!");
+// } else {
+//     console.log("OKEY!");
+// }
+
+
+// // Тернарные операторы!
+// (number > 200) ? console.log("Не верно!") : console.log("Верно")
+
+
+
+// switch
+let number = 100;
+switch (number) {
+    case 100: console.log("Верно!");
+        break;
+
+    case 60: console.log("Не верно!");
+        break;
+
+    default: console.log("Не в этот раз!");
+        break;
+}
