156278b5124c5d07dfb6432536fc2d346da53720
Sardor <mamirov.21@mail.ru>
2021-10-29 13:05:52 +0300

    first 10 prac - all good

---
+++ prac.js
@@ -0,0 +1,19 @@
+// let person = [];
+// person[0] = prompt("ты как урод?", "");
+// person[1] = prompt("какой сегодня день?", "");
+// person[2] = prompt("у тебя есть тачка?", "");
+
+// document.write(person);
+
+
+
+// let int = 10,
+//     float = 19.2;
+
+// console.log(int,  float);
+
+
+// let dsa = false,
+//     sda = false;
+
+//  console.log(dsa || sda); 
